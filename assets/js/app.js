e3222ce087a83e659427906598d80c81ec9e9249
leeyongbin <lyb@leeyongbinui-MacBookPro.local>
2026-08-06 14:31:00 +0900

    초기 커밋: 마즈/켈라노바 버전 (M1 완료)

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+const pages = {
+    arch: "pages/system_architecture.html",
+    data: "pages/data_layer_schema.html",
+    m1: "pages/m1_ai_shelf_scoring_mobile.html",
+    m2: "pages/m2_oos_alert_list_mobile.html",
+    m3: "pages/m3_integrated_dashboard.html",
+    m4: "pages/m4_ar_capture_guide.html",
+    m5: "pages/m5_competitor_comparison.html",
+    m6: "pages/m6_customer_recommendation_nudge.html",
+    m7: "pages/m7_roi_engine_ranking.html",
+    m8: "pages/m8_splh_workforce_scheduling.html",
+    exec: "pages/executive_dashboard_mockup.html"
+};
